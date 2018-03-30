27f999d5cd9fb8653b784e6a754bc1cc4229aa1d
shantypower <shantypower4891@yandex.ru>
2018-03-30 15:09:09 +0900

    Добавление карты Google

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function initialize() {
+  var mapOptions = {
+    zoom: 16,
+    center: new google.maps.LatLng(59.938143, 30.327466)
+  }
+  var map = new google.maps.Map(document.getElementById('map__canvas'),
+                                mapOptions);
+  var image = "../img/pin.svg";
+  var myLatLng = new google.maps.LatLng(59.938625, 30.322938);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    icon: image
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
