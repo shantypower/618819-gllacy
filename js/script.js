1c33e61faff6b82aef6eb3d60b635e2700923525
shantypower <shantypower4891@yandex.ru>
2018-03-31 21:12:21 +0900

    Исправление поведения полей и IE

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function initialize() {
   }
   var map = new google.maps.Map(document.getElementById('map__canvas'),
                                 mapOptions);
-  var image = "../img/pin.svg";
+  var image = "img/pin.svg";
   var myLatLng = new google.maps.LatLng(59.938625, 30.322938);
   var beachMarker = new google.maps.Marker({
     position: myLatLng,
@@ -14,3 +14,13 @@ function initialize() {
   });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+  var link = document.querySelector(".feedback__btn");
+  var popup = document.querySelector(".modal-login");
+  var close = popup.querySelector(".modal-close");
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+  });
