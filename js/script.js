824cc6c177a870aa774ad33505cdc93a8f0893a8
shantypower <shantypower4891@yandex.ru>
2018-04-01 14:01:24 +0900

    Правка футера и адресного блока по ПП. Замена id на классы

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function initialize() {
     zoom: 16,
     center: new google.maps.LatLng(59.938143, 30.327466)
   }
-  var map = new google.maps.Map(document.getElementById('map__canvas'),
+  var map = new google.maps.Map(document.getElementById("map__canvas"),
                                 mapOptions);
   var image = "img/pin.svg";
   var myLatLng = new google.maps.LatLng(59.938625, 30.322938);
@@ -13,6 +13,6 @@ function initialize() {
     icon: image
   });
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, "load", initialize);
 
 
