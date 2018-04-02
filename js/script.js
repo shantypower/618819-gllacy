90dea0179d916a2d845463533293d978bfca4440
shantypower <shantypower4891@yandex.ru>
2018-04-02 19:00:51 +0900

    Правка иконок соцсетей в футере: удалены псевдоэлементы. Добавлены фоновым изображением

--- js/script.js
+++ js/script.js
@@ -60,7 +60,6 @@ google.maps.event.addDomListener(window, "load", initialize);
       popup.classList.remove("modal-error");
       popup.offsetWidth = popup.offsetWidth;
       popup.classList.add("modal-error");
-      console.log("Необходимо заполнить все поля!");
     } else {
       if (isStorageSupport) {
         localStorage.setItem("login", login.value);
